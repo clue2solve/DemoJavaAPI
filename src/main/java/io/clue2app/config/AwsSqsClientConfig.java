8b0d6ab0f1702853b4ead2dada1043b5f46658ef
MLUNGU <mihail.lungu@clue2solve.com>
2025-07-30 12:43:39 +0300

    use DefaultAWSCredentials

--- src/main/java/io/clue2app/config/AwsSqsClientConfig.java
+++ src/main/java/io/clue2app/config/AwsSqsClientConfig.java
@@ -2,6 +2,7 @@ package io.clue2app.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import io.clue2solve.parameters.C2aConfig;
@@ -41,9 +42,11 @@ public class AwsSqsClientConfig {
 
         return AmazonSQSAsyncClientBuilder.standard()
                 .withRegion(region == null || region.isEmpty() ? "us-west-2" : region)
-                .withCredentials(new AWSStaticCredentialsProvider(
-                        new BasicAWSCredentials(queueConfig.get("accessKey"), queueConfig.get("secretKey"))
-                ))
+                .withCredentials(new DefaultAWSCredentialsProviderChain()
+/*
+                new AWSStaticCredentialsProvider(new BasicAWSCredentials(queueConfig.get("accessKey"), queueConfig.get("secretKey")))
+*/
+                )
                 .build();
     }
 
