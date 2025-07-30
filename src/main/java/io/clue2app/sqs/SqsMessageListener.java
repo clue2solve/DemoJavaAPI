2dbc59822d494ffd0e47b2554b68f5457a7ddd62
MLUNGU <mihail.lungu@clue2solve.com>
2025-07-30 14:15:05 +0300

    fix for SqsMessageListener

--- src/main/java/io/clue2app/sqs/SqsMessageListener.java
+++ src/main/java/io/clue2app/sqs/SqsMessageListener.java
@@ -1,10 +1,16 @@
 package io.clue2app.sqs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SqsMessageListener {
 
-    @SqsListener("your-queue-name")
+    @Value("${clue2app.queue.name}")
+    private String queueName;
+
+    @SqsListener("#{SqsMessageListener.queueName}")
     public void receiveMessage(String message) {
         System.out.println("Received message: " + message);
     }
