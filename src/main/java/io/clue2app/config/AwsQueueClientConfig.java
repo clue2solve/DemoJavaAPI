9193837a4065aa90679fc2735c3b3fd470a68ae7
MLUNGU <mihail.lungu@clue2solve.com>
2025-08-07 15:29:29 +0300

    sqs - test role injection

--- src/main/java/io/clue2app/config/AwsQueueClientConfig.java
+++ src/main/java/io/clue2app/config/AwsQueueClientConfig.java
@@ -2,6 +2,7 @@ package io.clue2app.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import io.clue2solve.parameters.C2aConfig;
@@ -44,10 +45,8 @@ public class AwsQueueClientConfig {
         return AmazonSQSAsyncClientBuilder.standard()
                 .withRegion(region == null || region.isEmpty() ? "us-west-2" : region)
                 .withCredentials(
-/*
                     new DefaultAWSCredentialsProviderChain()
-*/
-                    new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.get("accessKey"), config.get("secretKey")))
+//                    new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.get("accessKey"), config.get("secretKey")))
                 )
                 .build();
     }
